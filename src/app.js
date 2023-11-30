1424dd75d46794d53e44c22f6a9247864b5d85c8
shashwat pal <shashwatpal1021@gmail.com>
2023-11-30 19:58:32 +0530

    swagger update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 import express from "express";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "A sample API for learning Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import customerRouter from "./routes/customer.route.js";
-import genresRouter from "./routes/genres.route.js"
-import moviesRouter from "./routes/movies.routes.js"
-import rentalRouter from "./routes/rental.route.js"
+import genresRouter from "./routes/genres.route.js";
+import moviesRouter from "./routes/movies.routes.js";
+import rentalRouter from "./routes/rental.route.js";
 
 // app.use("/api/genres", genres);
 
--- src/routes/customer.route.js
+++ src/routes/customer.route.js
@@ -7,9 +7,18 @@ import {
 
 const router = Router();
 
+// routes/customer.route.js
+
+/**
+ * @swagger
+ * /customer:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ */
+
 router.route("/").get(getCustomer);
 router.route("/create").post(createCustomer);
 router.route("/:id").put(updateCustomer);
 
 export default router;
-
